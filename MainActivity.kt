5fe4539609e6e747f976840c2710228b459633a7
NMB2002 <138249391+NMB2002@users.noreply.github.com>
2023-07-02 00:45:53 +0530

    Update MainActivity.kt

--- MainActivity.kt
+++ MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.sqlitexandroiddemo
+package com.example.project
 
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
@@ -7,28 +7,85 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 
-
 class MainActivity : AppCompatActivity() {
+
+//    private lateinit var fname : EditText
+//    private lateinit var lname : EditText
+//    private lateinit var houseno : EditText
+//    private lateinit var city : EditText
+//    private lateinit var state : EditText
+//    private lateinit var pincode : EditText
+//    private lateinit var phoneno : EditText
+//    private lateinit var email : EditText
+//    private lateinit var manufacturer : EditText
+//    private lateinit var model : EditText
+//    private lateinit var year : EditText
+//    private lateinit var submitbtn: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val dbHelper = DBHelper(applicationContext)
-        val submitbtn = this.findViewById<Button>(R.id.submitBtn);
-        val viewBtn = this.findViewById<Button>(R.id.viewDB);
+        setContentView(R.layout.activity_registration_form)
+
+        var dbHelper = DBHelper (applicationContext)
+
+        val submitbtn = this.findViewById<Button>(R.id.submitbtn);
+
         fun register():Boolean? {
-            val name = this.findViewById<EditText>(R.id.nameTxt);
-            val mobno = this.findViewById<EditText>(R.id.mobno);
+            val fname = this.findViewById<EditText>(R.id.fname);
+            val lname = this.findViewById<EditText>(R.id.lname);
+            val houseno = this.findViewById<EditText>(R.id.houseno);
+            val city = this.findViewById<EditText>(R.id.city);
+            val state = this.findViewById<EditText>(R.id.state);
+            val pincode = this.findViewById<EditText>(R.id.pincode);
+            val phoneno = this.findViewById<EditText>(R.id.phoneno);
             val email = this.findViewById<EditText>(R.id.email);
-            val age = this.findViewById<EditText>(R.id.age);
-            return dbHelper.registerUser(mobno.text.toString(), name.text.toString(), email.text.toString(), age.text.toString());
+            val manufacturer = this.findViewById<EditText>(R.id.manufacturer);
+            val model = this.findViewById<EditText>(R.id.model);
+            val year = this.findViewById<EditText>(R.id.year);
+
+            return dbHelper.registerUser(
+                phoneno.text.toString(),
+                fname.text.toString(),
+                lname.text.toString(),
+                houseno.text.toString(),
+                city.text.toString(),
+                state.text.toString(),
+                pincode.text.toString(),
+                email.text.toString(),
+                manufacturer.text.toString(),
+                model.text.toString(),
+                year.text.toString());
+
         }
+
+
         submitbtn.setOnClickListener {
-            if(register() == true) Toast.makeText(applicationContext, "Successfully Registered", Toast.LENGTH_SHORT).show()
-            else Toast.makeText(applicationContext, "Unable to register!", Toast.LENGTH_SHORT).show()
-        }
-        viewBtn.setOnClickListener {
-            val viewDBIntent = Intent(this, DatabaseView::class.java)
-            startActivity(viewDBIntent)
+
+            val success = "Successfully Registered"
+            if (register() == true) {
+                Toast.makeText(
+                    applicationContext,
+                    success,
+                    Toast.LENGTH_SHORT
+                ).show()
+
+                var intent = Intent(this, success::class.java)
+                startActivity(intent)
+
+            }
+
+            else {
+                Toast.makeText(applicationContext, "Unable to register!", Toast.LENGTH_SHORT)
+                    .show()
+                val intent = Intent(this, success::class.java)
+                startActivity(intent)
+            }
+
         }
     }
+
 }
+
+
+
+
